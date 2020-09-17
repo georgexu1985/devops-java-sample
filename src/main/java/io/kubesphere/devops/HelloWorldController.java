050454721c55f7fcc3deb55024beb6630c72aad7
xujingji <xujj86@126.com>
2020-09-17 18:42:18 +0800

    增加端口

--- src/main/java/io/kubesphere/devops/HelloWorldController.java
+++ src/main/java/io/kubesphere/devops/HelloWorldController.java
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class HelloWorldController {
+	private static int count = 0;
 
     @RequestMapping("/hanyun/get")
     public String sayHello() {
+		System.out.println("#####################################" + count++);
         return "Really appreciate your star, that's the power of our life.";
     }
 }
